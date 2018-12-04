0789f58b5aff0f1d0f197b2174a986a223f32026
nchauhan890 <39838120+nchauhan890@users.noreply.github.com>
2018-12-04 19:30:38 +0000

    add project files + documentation

---
+++ AnonymousFile.py
@@ -0,0 +1,168 @@
+"""Create an anonymous file for testing code."""
+
+import sublime
+import sublime_plugin
+
+import os
+import random
+import string
+import sys
+import functools
+
+token_chars = string.ascii_letters + string.digits
+file_names = set()
+
+
+def catch_settings_exception(func):
+    """Catch exceptions in setting changes."""
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        try:
+            r = func(*args, **kw)
+        except Exception as e:
+            sublime.status_message('{} error in '
+                                  'AnonymousFile.sublime-settings:  {}'
+                                  .format(type(e).__name__, e))
+            print('AnonymousFile:', e.__class__.__name__, e)
+            raise
+            sys.exit(1)
+        else:
+            return r
+    return wrapper
+
+
+@catch_settings_exception
+def change_dir():
+    """Update 'dir' setting."""
+    global dir_, variables
+    dir_ = os.path.normpath(
+        sublime.expand_variables(settings.get('dir'), variables)
+    )
+    if not os.path.isdir(dir_):
+        raise TypeError('"dir" must be an existing directory')
+
+
+@catch_settings_exception
+def change_recent():
+    """Update 'recently_closed' setting."""
+    global recent, variables
+    recent = os.path.normpath(
+        sublime.expand_variables(settings.get('recently_closed'), variables)
+    )
+    if not os.path.isdir(dir_):
+        raise TypeError('"recently_closed" must be an existing directory')
+
+
+@catch_settings_exception
+def change_keep():
+    """Update 'keep' setting."""
+    global keep
+    keep = int(settings.get('keep'))
+    if keep < 1:
+        raise TypeError('"keep" must be greater than or equal to 0')
+    if keep > 100:
+        raise TypeError('"keep" cannot be more than 100')
+
+
+@catch_settings_exception
+def change_extension():
+    """Update 'file_extension' setting."""
+    global extension
+    extension = settings.get('file_extension')
+    if extension is None:
+        raise TypeError('must give a file extension')
+
+
+@catch_settings_exception
+def plugin_loaded():
+    """Set global variables upon loading."""
+    global settings, dir_, recent, keep, extension
+    global recent_file_names, variables
+    settings = sublime.load_settings(
+        'AnonymousFile.sublime-settings'
+    )
+    dir_ = settings.get('dir')
+    recent = settings.get('recently_closed')
+    keep = int(settings.get('keep'))
+    extension = settings.get('file_extension')
+
+    settings.add_on_change('dir', change_dir)
+    settings.add_on_change('recently_closed', change_recent)
+    settings.add_on_change('keep', change_keep)
+    settings.add_on_change('file_extension', change_extension)
+
+    variables = sublime.active_window().extract_variables()
+    dir_ = os.path.normpath(sublime.expand_variables(dir_, variables))
+    recent = os.path.normpath(sublime.expand_variables(recent, variables))
+
+    with open(os.path.join(dir_, 'recent_files.txt'), 'r+') as f:
+        recent_file_names = [line.strip() for line in f]
+
+
+class AnonymousFileCommand(sublime_plugin.TextCommand):
+    """Create anonymousfile."""
+
+    def run(self, edit):
+        """Run anonymous_file command."""
+        global file_names
+        window = self.view.window()
+        view = self.view
+
+        file_token = ''.join([random.choice(token_chars)
+                              for _ in range(3)])
+
+        file_name = 'af_' + file_token + extension
+        file_path = os.path.normpath(dir_) + os.path.sep + file_name
+        file = open(file_path, 'x')
+        file_names.add(file_name)
+        print('AnonymousFile: created', file_name)
+        print('AnonymousFile: recently closed files:')
+        print('\n'.join(recent_file_names))
+
+        file_view = window.open_file(file_path)
+        file.close()
+        window.focus_view(file_view)
+        view.run_command('new_dir')
+
+
+class CloseFileCommand(sublime_plugin.EventListener):
+    """Handle file closing/saving."""
+
+    def on_close(self, view):
+        """Close anonymous files."""
+        path = view.file_name()
+        file_path, file = os.path.split(path)
+
+        if file in file_names and dir_ == file_path:
+            try:
+                os.rename(path, os.path.join(recent, file))
+                while len(os.listdir(recent)) > keep and recent_file_names:
+                    try:
+                        os.remove(os.path.join(
+                                  recent, recent_file_names.pop(0)))
+                    except FileNotFoundError:
+                        pass
+
+                file_names.remove(file)
+                recent_file_names.append(file)
+                print('AnonymousFile: closed', file)
+                print('AnonymousFile: recently closed files:')
+                print('\n'.join(recent_file_names))
+                with open(os.path.join(dir_, 'recent_files.txt'), 'w+') as f:
+                    for i in recent_file_names:
+                        f.write(i + '\n')
+
+            except Exception as e:
+                print('AnonymousFile:', type(e), e)
+
+
+class SaveAnonymousFileCommand(sublime_plugin.TextCommand):
+    """Save anonymous file in a new directory."""
+
+    def run(self, edit):
+        """Save file elsewhere instead of deleting upon closure."""
+        path = self.view.file_name()
+        file_path, file = os.path.split(path)
+        if file in file_names and dir_ == file_path:
+            self.view.run_command('prompt_save_as')
+            file_names.remove(file)
--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# anonymous-file
+# Anonymous File
+
 Sublime Text 3 plugin to quickly create already-saved files to test code snippets.
+
+
+### Intro
+
+Have you ever thought of a brilliant idea and wanted to test out your code as fast as possible? You'd go through making a new file, writing the code out, saving the file somewhere, running it and then deleting it if the code failed. Now, with Anonymous File, you can quicken this workflow by creating pre-saved files which delete themselves upon closure.
+
+
+### Configuration
+
+Edit the AnonymousFile.sublime-settings files to configure this plugin. Open through `Preferences --> Package Settings --> AnonymousFile --> Settings - User` or the command palette.
+
+Setting                                  | Description
+----------------------------------------:|:------------------
+`dir`    | Directory to save new files
+`recently_closed` | Directory to move recently closed files to
+`keep`   | Number of recently closed files to keep - must be between 0 and 100
+`file_extension`  | Extension to create new files with - must contain the dot "." (e.g. ".py")
+
+If the plugin detects any errors in the settings, such as numbers out of boundaries or file system locations which aren't directories or do not exist, a message will be shown in the status bar and in the console.
+
+
+### Commands
+
+Anonymous File has 2 commands and 1 to edit settings available through the command palette. The table below shows the internal command names.
+
+
+Command                      | Command name
+----------------------------:|:-----------------
+Create Anonymous File        | anonymous_file
+Save Anonymous File          | save_anonymous_file
+
+
+### Context Menu
+
+Anonymous File provides access to the `Create Anonymous File` and `Save Anonymous File` commands by default with their respective names unchanged.
+
+
+### Usage and internals
+
+To create a new Anonymous File, right-click the current view to open the Context Menu and click `Create Anonymous File`, or alternatively click the same command through the Command Palette. This file is saved in the directory set as `dir` in the AnonymousFile.sublime-settings file and has the extension designated by the `file_extension` setting also in the settings file mentioned. The file can be used as normal and any code written can be run without having to save the file beforehand.
+
+Upon closure, the file is automatically deleted and moved to the directory set as `recently_closed` in the settings file. The `keep` setting specifies how many files will be kept in this directory; if the number is 0, the directory will remain empty. However, due to the nature of the code, the file is moved to the directory before being deleted so the `recently_closed` directory must be given regardless.
+
+When an Anonymous File is closed, it's file name is written to `recent_files.txt` which is located in the directory specified as `dir` in settings. The order of the file names are oldest to newest. The plugin writes this data to a file so that when the whole Sublime Text application is closed, the file names are kept in order to ensure the oldest file(s) is/are removed from the `recently closed` folder.
+
+Alternatively, the `Save Anonymous File` command can be invoked through the Context Menu or Command Palette which opens a `Save As` prompt to save the file. After saving, the file is removed from the plugin's list of currently open Anonymous Files so when closed, the file isn't deleted. Even if you save the file with the same `AF_xyz` name in the same directory, it still counts as saving the file and thus will not be deleted upon closure. 
